bf481b3e4a27c0f13ccc65e25018c30e48e09f36
Nicholas Ng <nicholasnkk@gmail.com>
2023-07-08 21:14:23 +0800

    added gitignore
    pasued at step 3 of https://developer.apple.com/tutorials/app-dev-training/using-stacks-to-arrange-views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Nicholas Ng on 8/7/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
